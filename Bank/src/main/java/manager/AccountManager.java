2395a32cc78bddfac51866a03c79330941aa98c2
redfad <redfad@github.com>
2019-01-23 12:55:05 +0000

    find operation by client

--- Bank/src/main/java/manager/AccountManager.java
+++ Bank/src/main/java/manager/AccountManager.java
@@ -1,40 +1,91 @@
 package manager;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import dao.AccountDAO;
+import dao.TransactionDAO;
 import exception.BankAccountException;
 import model.Account;
+import model.Client;
+import model.Transaction;
+import model.TransactionStatus;
 
 public class AccountManager {
 	private final Logger LOG = Logger.getLogger("AccountManager");
 	private final AccountDAO accountDAO = new AccountDAO();
+	private final TransactionDAO transactionDAO = new TransactionDAO();
 
-	public Account depositAmount(Account account, BigDecimal amount) throws BankAccountException {
-		LOG.info("Request to deposit amount : " + amount + " to Account : " + account);
-		if (account != null) {
+	/**
+	 * Deposit an amount
+	 * 
+	 * @param client
+	 * @param account
+	 * @param amount
+	 * @return
+	 * @throws BankAccountException
+	 */
+	public Account depositAmount(Client client, Account account, BigDecimal amount) throws BankAccountException {
+		LOG.info("Request to deposit amount : " + amount + " to Account : " + account + " by client :" + client);
+		Transaction tx;
+		if (account != null && client != null) {
+			tx = new Transaction(account, client, TransactionStatus.DEPOSIT, amount);
 			account.setBalance(account.getBalance().add(amount));
 			accountDAO.save(account);
+			transactionDAO.save(tx);
 		} else {
-			throw new BankAccountException("Please provide an account not null !");
+			throw new BankAccountException("Please provide an account or client not null !");
 		}
 		return account;
 	}
 
-	public Account withdrawAmount(Account account, BigDecimal amount) throws BankAccountException {
-		LOG.info("Request to deposit amount : " + amount + " to Account : " + account);
-		if (account != null) {
+	/**
+	 * Withdraw an amount
+	 * 
+	 * @param client
+	 * @param account
+	 * @param amount
+	 * @return
+	 * @throws BankAccountException
+	 */
+	public Account withdrawAmount(Client client, Account account, BigDecimal amount) throws BankAccountException {
+		LOG.info("Request to deposit amount : " + amount + " to Account : " + account + " by client :" + client);
+		Transaction tx;
+		if (account != null && client != null) {
 			if (account.getBalance().compareTo(amount) < 0) {
 				throw new BankAccountException("insufficient balance! current balance :" + account.getBalance());
 			} else {
+				tx = new Transaction(account, client, TransactionStatus.WITHDRAW, amount);
 				account.setBalance(account.getBalance().subtract(amount));
 				accountDAO.save(account);
+				transactionDAO.save(tx);
 			}
 		} else {
-			throw new BankAccountException("Please provide an account not null !");
+			throw new BankAccountException("Please provide an account or client not null !");
 		}
 		return account;
+	}
+
+	/**
+	 * get Operations by client
+	 * @param client
+	 * @return
+	 * @throws BankAccountException
+	 */
+	public List<Transaction> getOperations(Client client) throws BankAccountException{
+		List<Transaction> operations;
+		if(client==null) {
+			throw new BankAccountException("Please provide a client not null !");
+		}else {
+			operations = transactionDAO.findAll().
+					stream().
+					filter(transaction -> transaction.getClient().getClientId().equals(client.getClientId())).
+					collect(Collectors.toList());
+		}
+		
+		return operations;
 	}
 
 }
--- Bank/src/test/java/manager/AccountManagerTest.java
+++ Bank/src/test/java/manager/AccountManagerTest.java
@@ -12,9 +12,10 @@ import org.junit.jupiter.api.Test;
 
 import exception.BankAccountException;
 import model.Account;
+import model.Client;
 
 class AccountManagerTest {
-	
+
 	static AccountManager manager;
 
 	@BeforeAll
@@ -37,31 +38,51 @@ class AccountManagerTest {
 	@Test
 	void shouldDepositAmount() throws BankAccountException {
 		Account account = new Account();
-		account = manager.depositAmount(account, new BigDecimal(20.0));
+		Client client = new Client("bob", "marley", "bob123");
+		account = manager.depositAmount(client, account, new BigDecimal(20.0));
 		assertEquals(account.getBalance(), new BigDecimal(20.0));
-		
+
 	}
-	
+
 	@Test
 	void shouldFailWhenAccountIsNotProvided() throws BankAccountException {
 		assertThrows(BankAccountException.class, () -> {
-			manager.depositAmount(null, new BigDecimal(20.0));
-	    });
+			manager.depositAmount(null, null, new BigDecimal(20.0));
+		});
 	}
-	
+
 	@Test
 	void shouldWithdrawAmount() throws BankAccountException {
 		Account account = new Account(new BigDecimal(20.0));
-		account = manager.withdrawAmount(account, new BigDecimal(10.0));
+		Client client = new Client("bob", "marley", "bob123");
+		account = manager.withdrawAmount(client, account, new BigDecimal(10.0));
 		assertEquals(account.getBalance(), new BigDecimal(10.0));
 	}
-	
+
 	@Test
 	void shouldFailWithdrawAmountInsufficientBalance() throws BankAccountException {
 		Account account = new Account(new BigDecimal(10.0));
+		Client client = new Client("bob", "marley", "bob123");
 		assertThrows(BankAccountException.class, () -> {
-			manager.withdrawAmount(account, new BigDecimal(20.0));
-	    });
+			manager.withdrawAmount(client, account, new BigDecimal(20.0));
+		});
+	}
+
+	@Test
+	void shouldPrintOperationsByClientId() throws BankAccountException {
+		Account account = new Account(new BigDecimal(100.0));
+		Client client = new Client("bob", "marley", "bob123");
+
+		int oldsize = manager.getOperations(client).size();
+
+		manager.withdrawAmount(client, account, new BigDecimal(10.0));
+		manager.depositAmount(client, account, new BigDecimal(12.0));
+		manager.depositAmount(client, account, new BigDecimal(13.0));
+		manager.withdrawAmount(client, account, new BigDecimal(20.0));
+		manager.depositAmount(client, account, new BigDecimal(10.0));
+
+		int newSize = manager.getOperations(client).size();
+		assertEquals(newSize, oldsize + 5);
 	}
 
 }
